08e0d4050a027dfa75fedd01df73b3b30ed494b3
jasonlkh-wa <50670119+jasonlkh-wa@users.noreply.github.com>
2023-10-30 22:25:53 +0800

    Add doc-string to all functions.

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ import logging
 
 
 def dirname(file: str) -> str:
+    """Returns the directory name of a given file path."""
     return os.path.expanduser(os.path.dirname(file))
 
 
@@ -36,6 +37,7 @@ def load_and_describe_df(path: str, sheet_name=0) -> pd.DataFrame:
 
 
 def mach(x: str):
+    """Replaces spaces and hyphens in a string with underscores."""
     return x.replace(" ", "_").replace("-", "_")
 
 
@@ -54,6 +56,72 @@ def setup_main_logger():
     return logger
 
 
+format_codes = {
+    "format": {
+        "reset": "\033[0m",
+        "bold": "\033[1m",
+        "dim": "\033[2m]",
+        "italic": "\033[3m",
+        "underline": "\033[4m",
+        "strikethrough": "\033[9m",
+        "reverse": "\033[7m",
+    },
+    "foreground_color": {
+        "black": "\033[30m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+    },
+    "background_color": {
+        "black": "\033[40m",
+        "red": "\033[41m",
+        "green": "\033[42m",
+        "yellow": "\033[43m",
+        "blue": "\033[44m",
+        "magenta": "\033[45m",
+        "cyan": "\033[46m",
+        "white": "\033[47m",
+        "default": "\033[49m",
+    },
+}
+
+
+def get_highlight_string(x, format: list = ["reset"], fg="yellow", bg="default") -> str:
+    """
+    Generate a highlighted string with specified formatting options.
+
+    Args:
+        x (Any): The string to be highlighted.
+        format (list, optional): A list of formatting options. Defaults to ["reset"].
+        fg (str, optional): The foreground color. Defaults to "yellow".
+        bg (str, optional): The background color. Defaults to "default".
+
+    Returns:
+        str: The highlighted string.
+
+    """
+    prefix = (
+        "".join([f"{format_codes['format'][i]}" for i in format])
+        + format_codes["foreground_color"][fg]
+        + format_codes["background_color"][bg]
+    )
+    return f"{prefix}{x}\033[0m"
+
+
+def investigate(x):
+    """Prints the type and value of the input string."""
+    printls(f"type: {type(x)}\nvalue:\n {x=}")
+
+
+def printls(x: str, n=50):
+    """Print a line separator followed by the input string and another line separator."""
+    line_separator = "-" * n
+    print(f"{line_separator}\n{x}\n{line_separator}")
+
+
 if __name__ == "__main__":
-    print(help(pd.read_csv))
     pass
