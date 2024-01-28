bced9a2a0b4e2c044970fef8958a863826d17513
jasonlkh-wa <50670119+jasonlkh-wa@users.noreply.github.com>
2024-01-28 15:25:17 +0800

    Add [Record] class factory to [whelper.t]

---
+++ t/record.py
@@ -0,0 +1,35 @@
+# CR-someday: Improve all the intra-package import and dependency management
+from whelper import printls
+
+
+def create_record_type(record: dict[str, type], type_enforced=True):
+    doc_string = ",\n\t".join([f"{k}: {type_t}" for k, type_t in record.items()])
+
+    class Record:
+        __doc__ = f"""{doc_string}"""
+
+        def __init__(self, **kwargs):
+            if type_enforced:
+                self._init_with_type_enforced(**kwargs)
+            else:
+                for k, type_t in record.items():
+                    setattr(self, k, kwargs[k])
+
+        def _init_with_type_enforced(self, **kwargs):
+            if not record.keys() == kwargs.keys():
+                raise TypeError(f"expected:\n{record.keys()}\n\ngot:\n{kwargs.keys()}")
+            for k, type_t in record.items():
+                if isinstance(kwargs[k], type_t):
+                    setattr(self, k, kwargs[k])
+                else:
+                    try:
+                        setattr(self, k, type_t(kwargs[k]))
+                    except ValueError:
+                        printls(f"Expected {type_t} for {k}, got {type(kwargs[k])}")
+                        raise TypeError("Unexpected field type")
+
+        def __str__(self):
+            class_name = self.__class__.__name__
+            return f"{class_name} = {{\n\t{doc_string}\n\t}}".expandtabs(4)
+
+    return Record
---
+++ t/test_record.py
@@ -0,0 +1,34 @@
+from . import create_record_type
+
+
+def test_create_record_type_success():
+    # Testing creation of record type with correct input
+    assert create_record_type({"a": int, "b": str})
+
+
+def test_create_record_type_with_type_enforced():
+    # Testing creation of record type with type enforcement
+    Type = create_record_type({"a": int, "b": str})
+    try:
+        Type(a="A", b="2")
+    except TypeError:
+        assert True
+        return None
+
+    assert False
+
+
+def test_create_record_type_without_type_enforced():
+    Type = create_record_type({"a": int, "b": str}, type_enforced=False)
+    assert Type(a="A", b="2")
+
+
+def test_create_record_type_print():
+    Type = create_record_type({"a": int, "b": str})
+    assert (
+        str(Type(a=1, b="2"))
+        == """Record = {
+    a: <class 'int'>,
+    b: <class 'str'>
+    }"""
+    )
