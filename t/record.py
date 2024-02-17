484fb48f6b94d77cf89b75f6ed9242d8b247103d
jasonlkh-wa <50670119+jasonlkh-wa@users.noreply.github.com>
2024-02-17 21:26:15 +0800

    add [set_*] methods for [t.Record] to force the type checking when attrs are updated

--- t/record.py
+++ t/record.py
@@ -1,4 +1,5 @@
 from whelper import raisels
+import types
 
 
 class Record:
@@ -8,7 +9,6 @@ class Record:
 # CR: add set_* method, and change all field to _field
 def create_record_type(
     record: dict[str, type | set],
-    type_enforced=True,
     type_caster_dict: dict | None = None,
 ):
     doc_string = ",\n\t".join([f"{k}: {type_t}" for k, type_t in record.items()])
@@ -16,17 +16,69 @@ def create_record_type(
     class _Record(Record):
         __doc__ = f"""{doc_string}"""
         RECORD = record
+        TYPE_CASTER_DICT = type_caster_dict if type_caster_dict is not None else dict()
 
         def __init__(self, **kwargs):
-            if type_enforced:
-                self.type_caster_dict = (
-                    type_caster_dict if type_caster_dict is not None else dict()
+            self._init_with_type_enforced(**kwargs)
+
+        def __setattr__(self, name, value, allowed=False):
+            # To block any assignment after [__init__]
+            if not allowed:
+                raisels(
+                    AttributeError,
+                    "Direct attribute asisgnment is not allowed\n"
+                    "Use setter methods or create a new record instead",
                 )
 
-                self._init_with_type_enforced(**kwargs)
-            else:
-                for k in self.RECORD.keys():
-                    setattr(self, k, kwargs[k])
+            # set [set_*] method
+            if name.startswith("set_") and name.removeprefix("set_") in self.RECORD:
+                super().__setattr__(name, value)
+                return None
+
+            try:
+                type_t = self.RECORD[name]
+            except KeyError:
+                # RECORD and TYPE_CASTER_DICT should not be set after initialized
+                raisels(
+                    AttributeError,
+                    f"Attribute [{name}] is not found in {self.RECORD}",
+                )
+
+            # If type_t is a set of values, set the attribute if [value] is in [type_t]
+            if isinstance(type_t, set):
+                if value in type_t:
+                    super().__setattr__(name, value)
+                    return None
+                else:
+                    return TypeError(
+                        f"Value [{value}] is not one of the variants:\n\n{type_t}"
+                    )
+
+            # Set the attribute if [value] matches [type_t]
+            if isinstance(value, type_t):
+                super().__setattr__(name, value)
+                return None
+
+            # Try to convert [value] to [type_t] and set the attribute
+            try:
+                super().__setattr__(
+                    name,
+                    self.TYPE_CASTER_DICT.get(name, type_t)(value),
+                )
+                return None
+            except (TypeError, ValueError):
+                return TypeError(f"Expected {type_t} for {value}, got {type(value)}")
+
+        def setattr(self, name, value):
+            return self.__setattr__(name, value, allowed=True)
+
+        def create_set_methods(self, name):
+            def set_method(self, value):
+                return self.setattr(name, value)
+
+            # CR-someday: this is forcing python to recognize it as method
+            # think of an alternative way
+            return types.MethodType(set_method, self)
 
         def _init_with_type_enforced(self, **kwargs):
             if not self.RECORD.keys() == kwargs.keys():
@@ -34,29 +86,12 @@ def create_record_type(
                     TypeError,
                     f"Expected:\n{self.RECORD.keys()}\n\ngot:\n{kwargs.keys()}",
                 )
-            for k, type_t in self.RECORD.items():
-                if isinstance(type_t, set):
-                    if kwargs[k] in type_t:
-                        setattr(self, k, kwargs[k])
-                    else:
-                        raisels(
-                            TypeError,
-                            f"Value [{kwargs[k]}] is not one of the variants:\n\n{type_t}",
-                        )
-
-                elif isinstance(kwargs[k], type_t):
-                    setattr(self, k, kwargs[k])
-
-                else:
-                    try:
-                        setattr(
-                            self, k, self.type_caster_dict.get(k, type_t)(kwargs[k])
-                        )
-                    except (TypeError, ValueError):
-                        raisels(
-                            TypeError,
-                            f"Expected {type_t} for {k}, got {type(kwargs[k])}",
-                        )
+            for name, value in kwargs.items():
+                error = self.setattr(name, value)
+                if error is not None:
+                    raisels(type(error), str(error))
+                print(name)
+                self.setattr(f"set_{name}", self.create_set_methods(name))
 
         def to_dict(self):
             return {k: getattr(self, k) for k in self.RECORD.keys()}
@@ -64,11 +99,7 @@ def create_record_type(
         def __repr__(self):
             class_name = self.__class__.__name__
             body = ",\n\t".join(
-                [
-                    f"{k}: {v}"
-                    for k, v in self.__dict__.items()
-                    if k not in {"type_caster_dict"}
-                ]
+                [f"{k}: {getattr(self, k)}" for k in self.RECORD.keys()]
             )
             return f"{class_name} = {{\n\t{body}\n}}".expandtabs(4)
 
--- t/test/test_record.py
+++ t/test/test_record.py
@@ -2,19 +2,27 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from record import create_record_type
+from record import create_record_type, Record
+
+SampleClass = create_record_type({"a": int, "b": str})
+sample_instance = SampleClass(a=1, b="2")
 
 
+# Testing creation of record type with correct input
 def test_create_record_type_success():
-    # Testing creation of record type with correct input
-    assert create_record_type({"a": int, "b": str})
+    assert issubclass(SampleClass, Record)
+    assert isinstance(sample_instance, Record)
 
 
-def test_create_record_type_with_type_enforced_and_wrong_type():
-    # Testing creation of record type with type enforcement
-    Type = create_record_type({"a": int, "b": str})
+def test_record_type_attributes_and_method():
+    expect_keys = {"a", "b", "set_a", "set_b"}
+    assert set(sample_instance.__dict__.keys()) == expect_keys
+
+
+# Testing creation of record type with type enforcement
+def test_create_record_type_with_wrong_type():
     try:
-        Type(a="A", b="2")
+        SampleClass(a="A", b="2")
     except TypeError:
         assert True
         return None
@@ -22,17 +30,36 @@ def test_create_record_type_with_type_enforced_and_wrong_type():
     assert False
 
 
-def test_create_record_type_without_type_enforced():
-    Type = create_record_type({"a": int, "b": str}, type_enforced=False)
-    assert Type(a="A", b="2")
-
-
-def test_create_record_type_print():
-    Type = create_record_type({"a": int, "b": str})
+def test_record_repr():
     assert (
-        str(Type(a=1, b="2"))
+        str(SampleClass(a=1, b="2"))
         == """_Record = {
     a: 1,
     b: 2
 }"""
     )
+
+
+def test_block_direct_assignment():
+    try:
+        sample_instance.a = "A"
+    except AttributeError:
+        assert True
+        return None
+    print(
+        "Expected AttributeError not raised, the class should block direct assignment"
+    )
+    assert False
+
+
+def test_set_attribute():
+    sample_instance = SampleClass(a=1, b="2")
+    sample_instance.set_a(3)
+    assert sample_instance.a == 3
+
+
+def test_set_attribute_with_wrong_type():
+    error = sample_instance.set_a("b")
+
+    assert isinstance(error, TypeError)
+    assert str(error) == "Expected <class 'int'> for b, got <class 'str'>"
